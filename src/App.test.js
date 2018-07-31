077d286a859e7b748ff8a30ee53a91b84f3330ed
Erik Eide <erik.eide@gmail.com>
2018-07-31 19:39:59 +1000

    Replace generated content with a title in <App>

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
+import Enzyme from 'enzyme';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('render', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = Enzyme.shallow(<App/>);
+  });
+
+  it('should render an App title', () => {
+    expect(wrapper.find({ testId: 'title' })).toHaveLength(1);
+  });
 });
